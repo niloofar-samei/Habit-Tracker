caac9e0ea0211d1896129a062ea940b3382f8c07
niloofar-samei <niloofar.samei@gmail.com>
2025-08-04 22:55:25 +0100

    Create HabitListCreateView.

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import permissions
+from .models import Habit, HabitRecord
+from .serializers import HabitSerializer, HabitRecordSerializer
 
-# Create your views here.
+
+class HabitListCreateView(generics.ListCreateView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
