d80766b3930c598ecd940220e44a4cf7ea6dc82f
niloofar-samei <niloofar.samei@gmail.com>
2025-08-05 20:50:09 +0100

    Create HabitRecordListCreateView.

--- tracker/views.py
+++ tracker/views.py
@@ -22,3 +22,19 @@ class HabitDetailVewi(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Habit.objects.filter(owner=self.request.user)
+
+
+class HabitRecordListCreateView(generics.ListCreateAPIView):
+    serializer_class = HabitRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        habit_id = self.kwargs["habit_id"]
+        return HabitRecord.objects.filter(
+            habit__id=habit_id, habit__owner=self.request.user
+        )
+
+    def perform_create(self, serializer):
+        habit_id = self.kwargs["habit_id"]
+        habit = Habit.objects.get(id=habit_id, owner=self.request.user)
+        serializer.save(habit=habit)
